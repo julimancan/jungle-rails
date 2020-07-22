bf991de51d84bda710f0ad3cddfe4133a6b03bc8
julimancan <julian.m.bustos@gmail.com>
2020-07-22 19:14:25 -0400

    added a table to admin dashboard with product count category count and a products link

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD']
 
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
