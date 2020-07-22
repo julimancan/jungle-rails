d247a11ea3015c13b427eaab7f909110fb5702ad
julimancan <julian.m.bustos@gmail.com>
2020-07-22 19:38:37 -0400

    added the about page and linked to it in the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
+  match 'about' => 'about#show', :via => :get
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
